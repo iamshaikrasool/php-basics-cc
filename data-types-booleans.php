e1fb95a14351e8ddddd21479f9c86272d908f898
Shaik Rasool <hello@shaikrasool.net>
2018-02-23 15:13:12 +0530

    first commit

---
+++ data-types-booleans.php
@@ -0,0 +1,29 @@
+<?php
+/*
+$fileIsUploaded = false;
+
+if ($fileIsUploaded) {
+    echo 'Your file is uploaded.';
+} else {
+    echo 'Your file isn\'t uploaded.';
+}
+----------------------------------------------------------------------
+$isInt = is_int(1);
+
+if ($isInt) {
+    echo 'This is an inteager.';    // This is an inteager.
+}
+-----------------------------------------------------------------------
+$isInt = is_int('Mueed');
+
+if ($isInt) {
+    echo 'This is an inteager.';    // blank
+}
+-----------------------------------------------------------------------
+var_dump(is_int(1));    // bool(true)
+var_dump(is_int('Mueed'));  // bool(false)
+----------------------------------------------------------------------
+*/
+
+var_dump(is_int(1));    // bool(true)
+var_dump(is_int('Mueed'));  // bool(false)
